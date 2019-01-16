03903abeb0e46e1bf07469b6335a569f139af03c
FUSIONEX\calvin.low.kw <calvin.low.kw@adv-fusionex.com>
2019-01-16 11:48:06 +0800

    Changed feault timezone to Kuala Lumpur

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Peatio
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = ENV.fetch('TIMEZONE')
+    config.time_zone = ENV.fetch('TIMEZONE', 'Kuala Lumpur')
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
